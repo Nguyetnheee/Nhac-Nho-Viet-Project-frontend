98d4e111e8579be4513c8f1fb3fc81693cb4fdeb
thnn <thnn16102005@gmail.com>
2025-10-25 22:09:59 +0700

    feat: update rituals lookup UI

--- src/pages/RitualLookup.js
+++ src/pages/RitualLookup.js
@@ -241,7 +241,7 @@ const RitualLookup = () => {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 md:gap-10">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 md:gap-10">
             {loading ? (
               // Hiển thị Skeleton Loading
               [...Array(8)].map((_, index) => (
